f7fbb0d6428d2cab9d7ac903c28c73f343e25280
Jason <jmaalegado@gmail.com>
2021-06-27 19:06:21 +1000

    - project started - cleaning up logic for table generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';   // Package containing methods we need for TD form processing
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './input-form/input-form.component';
+import { DescriptionComponent } from './description/description.component';
+import { OutputTableComponent } from './output-table/output-table.component';
+import { OutputChartComponent } from './output-chart/output-chart.component';
+import { OutputViewComponent } from './output-view/output-view.component';
+import { ListColumnComponent } from './list-column/list-column.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
+    DescriptionComponent,
+    OutputTableComponent,
+    OutputChartComponent,
+    OutputViewComponent,
+    ListColumnComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
